5e6aeabd165976ffcdacf8a5e966a61a37172e67
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-06-08 01:51:05 +0200

    added oauth+default url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  default_url_options :host => "localhost:3000"
+
   root to: 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
