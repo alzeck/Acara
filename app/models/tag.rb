9a77e6e166e415cd28825c7e59671b045c47752c
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-06-11 01:22:43 +0200

    add validations to Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,10 +1,10 @@
 class Tag < ApplicationRecord
 
   #Controlla che i seguenti campi non siano vuoti
-  validates :name, :presence => true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 
+  # Only allow letter, number, underscore and punctuation.
+  validates_format_of :name, with: /^#[a-z0-9_]*$/, :multiline => true
 
-  #Controlli sulle chiavi interne
-  validates_uniqueness_of :name
 
 end
