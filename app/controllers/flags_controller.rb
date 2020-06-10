785fdbaf0b560b97f43f27f702dc499946000efd
Matteo Di Stadio <distadio.1794111@studenti.uniroma1.it>
2020-06-10 23:05:19 +0200

    fatto controller dei flags

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -1,29 +1,80 @@
 class FlagsController < ApplicationController
 
-  # TO BE DECIDED DEPENDING ON WEBSOCKETS
-  #GET su /flags
-  def index
-  end
+	#GET su /flags
+	def index
+		if current_user.admin              
+			@flags = Flag.all
+		else
+			render_422
+		end
+	end
 
 
-  #POST su /flags
-  def create
-  end
+	#GET su /flags/:id
+	def show
+		id = params[:id]
+        
+		if Flag.exists?(id)
+			if current_user.admin                    
+				@flag = Flag.find(id)
+			else
+				render_422
+			end
+        else
+            render_404
+        end
+	end
 
 
-  # HTML MODAL
-  #GET su /flags/new
-  def new
-  end
+	#GET su /flags/new
+	def new
+		if ! user_signed_in?
+            render_422
+        end
+	end
 
 
-  #GET su /flags/:id
-  def show
-  end
+	#POST su /flags
+	def create
+		if user_signed_in?
+			flag = Flag.new(params[:flag].permit(:reason, :description, :url), user_id: current_user.id)
+
+			if flag.valid?
+				if ! flag.save
+					render_500
+				end
+			else
+				render_400
+			end
+		else
+			render_422
+		end 
+	end
 
 
-  #DELETE su /flags/:id
-  def destroy
-  end
+	#DELETE su /flags/:id
+	def destroy
+		if user_signed_in?
+            id = params[:id]
+            
+            if Flag.exists?(id)
+                flag = Flag.find(id)
+
+                if current_user.admin                    
+                    if flag.destroy
+                        redirect_to flags_path
+                    else
+                        render_500
+                    end
+                else
+                    render_422
+                end
+            else
+                render_404
+            end
+        else
+            render_422
+        end
+	end
   
 end
