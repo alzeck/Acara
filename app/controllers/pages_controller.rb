2ea048bc0765111e3430b773fdd0251ea0e06e89
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-06-13 02:08:16 +0200

    ui improvements
    - searchbar working
    - icons added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def search
+    @search = params[:q]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   default_url_options :host => "localhost:3000"
 
   root to: "pages#home"
+  get "/search", to: "pages#search", as: "search"
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
