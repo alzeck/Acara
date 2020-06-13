b6bff4299d1ea11e39f046632db9b8a3e17a7f86
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-06-13 11:19:59 +0200

    added has_map

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def has_navbar
     !["sessions", "registrations", "passwords", "confirmations", "unlocks"].include? controller_name
   end
+
+  def has_maps
+    ["events"].include? controller_name
+  end
+
 end
