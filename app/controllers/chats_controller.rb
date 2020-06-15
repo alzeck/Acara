eb4ee63cc93ecbe46f082f6c24ffa8a4388a1772
Matteo Di Stadio <distadio.1794111@studenti.uniroma1.it>
2020-06-15 18:46:28 +0200

    lavoro sui controller delle chat e dei messaggi

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,17 +1,56 @@
 class ChatsController < ApplicationController
-  # TO BE DECIDED DEPENDING ON WEBSOCKETS
+
+  #GET su /chats
   def index
-  end
-  
-  def create
+    if user_signed_in?
+      @chats = Chat.where("chats.user1_id = #{current_user.id} OR chats.user2_id = #{current_user.id}")
+    else
+      render_401
+    end
   end
 
-  def new
-  end
-  
+
+  #GET su /chats/:id
   def show
-  end
+    if user_signed_in?
+      if Chat.exists?(params[:id])
+        @chat = Chat.find(params[:id])
 
-  def destroy
+        if @chat.user1_id == current_user.id || @chat.user2_id == current_user.id
+          @messages = Message.where(chat_id: params[:id]).sort_by(&:created_at)
+        else
+          render_403
+        end
+
+      else
+        render_404
+      end
+    else
+      render_401
+    end
   end
+  
+
+  #POST su /chats
+  def create
+    if user_signed_in?
+      par = params[:chat].permit(:user_id)
+      primo_id = par[:where] > current_user.id ? par[:where] : current_user.id
+      secondo_id = par[:where] < current_user.id ? par[:where] : current_user.id
+      @chat = Chat.new(user1_id: primo_id, user2_id: secondo_id)
+
+      if @chat.valid?
+        if @chat.save
+          redirect_to chats_path
+        else
+          render_500
+        end
+      else
+        render_400
+      end
+    else
+      render_401
+    end
+  end
+  
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,23 @@
 class MessagesController < ApplicationController
+
+  #POST su /chats/:chat_id/messages
   def create
+    if user_signed_in?
+      par = params[:chat].permit(:content)
+      @message = Message.new(content: par[:content], chat_id: params[:chat_id], user_id: current_user.id)
+
+      if @message.valid?
+        if @message.save
+          redirect_to chats_path
+        else
+          render_500
+        end
+      else
+        render_400
+      end
+    else
+      render_401
+    end
   end
 
-  def new
-  end
-
-  def show
-  end
 end
