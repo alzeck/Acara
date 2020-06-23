dea224440f763e83bb1a29ab2a346540a34ec8f4
andrea-cerone <andreacerone97@yahoo.it>
2020-06-24 00:20:25 +0200

    modificate api di comments, events, follows e partecipation

--- app/controllers/api/participations_controller.rb
+++ app/controllers/api/participations_controller.rb
@@ -1,129 +1,86 @@
 class Api::ParticipationsController < ApplicationController
 
-	#GET su /events/:event_id/participations
-	def index
-		event_id = params[:event_id]
-		
-		if Event.exists?(event_id)			
-			if user_signed_in?
-				part = Participation.where(user_id: current_user.id, event_id: event_id)[0]
+  #POST su /api/events/:event_id/participations
+  def create
+    if user_signed_in?
+      event_id = params[:event_id]
+      par = params[:participation].permit(:value)
+      if Event.exists?(event_id)
+        participation = Participation.new(value: par[:value], user_id: current_user.id,
+                                          event_id: event_id)
 
-				if part.nil?
-					@participation = nil
-				else
-					@participation = part.value
-				end
-			else
-				@participation = nil
-			end			
-		else
-			render status: 404
-		end
-	end
-
-
-	#POST su /events/:event_id/participations
-	def create
-		if user_signed_in?
-			event_id = params[:event_id]
-			
-			if Event.exists?(event_id)
-				participation = Participation.new(params[:participation].permit(:value), user_id: current_user.id, event_id: event_id)
-
-				if participation.valid?
-					if participation.save
-						redirect_to api_event_path(Event.find(event_id))
-					else
-						render status: 500
-					end
-				else
-					render status: 400
-				end
-			else
-				render status: 404
-			end
-		else
-			render status: 422
-		end
-	end
-
-
-	#PATCH/PUT su /events/:event_id/participations/:id
-	def update
-		if user_signed_in?
-			event_id = params[:event_id]
-        
-			if Event.exists?(event_id)
-				participation_id = params[:id]
-				event = Event.find(event_id)
-				
-				if Participation.exists?(participation_id)
-					participation = Participation.find(participation_id)
-
-					if current_user.id == participation.user_id || current_user.admin
-						if participation.event_id == event_id
-							participation.assign_attributes(params[:participation].permit(:value), user_id: current_user.id, event_id: event_id)
-                    
-							if participation.valid?
-								if participation.save
-									redirect_to api_event_path(event)
-								else
-									render status: 500
-								end
-							else
-								render status: 400
-							end
-						else
-							render status: 400
-						end
-					else
-						render status: 422
-					end
-				else
-					render status: 404
-				end
-			else
-				render status: 404
-			end
-		else
-			render status: 422
-		end
-	end
-
-
-	#DELETE su /events/:event_id/participations/:id
-	def destroy
-		if user_signed_in?
-			event_id = params[:event_id]
-        
-			if Event.exists?(event_id)
-				participation_id = params[:id]
-				
-				if Participation.exists?(participation_id)
-					participation = Participation.find(participation_id)
-
-					if current_user.id == participation.user_id || current_user.admin
-						if participation.event_id == event_id
-							if participation.destroy
-								redirect_to api_event_path(Event.find(event_id))
-							else
-								render status: 500
-							end
-						else
-							render status: 400
-						end
-					else
-						render status: 422
-					end
-				else 
-					render status: 404
-				end
-			else
-				render status: 404
-			end
-		else
-			render status: 422
-		end
+        if participation.valid?
+          if participation.save!
+            render status: 200
+          else
+            render status: 500
+          end
+        else
+          render status: 400
+        end
+      else
+        render status: 404
+      end
+    else
+      render status: 401
     end
-    
+  end
+
+  #PATCH/PUT su /api/events/:event_id/participations/:id
+  def update
+    if user_signed_in?
+      event_id = params[:event_id].to_i
+      participation = Participation.where(id: params[:id])[0]
+
+      if !participation.nil? && participation.event_id == event_id
+        # check if the participation exists for the given event
+        # if it exists the event that contains it exists
+        if current_user.id == participation.user_id || current_user.admin
+          par = params[:participation].permit(:value)
+          participation.assign_attributes(value: par[:value])
+          if participation.valid?
+            if participation.save
+              render status: 200
+            else
+              render status: 500
+            end
+          else
+            render status: 400
+          end
+        else
+          render status: 403
+        end
+      else
+        render status: 404
+      end
+    else
+      render status: 401
+    end
+  end
+
+  #DELETE su /api/events/:event_id/participations/:id
+  def destroy
+    if user_signed_in?
+      event_id = params[:event_id].to_i
+      participation = Participation.where(id: params[:id])[0]
+
+      if !participation.nil? && participation.event_id == event_id
+        # check if the participation exists for the given event
+        # if it exists the event that contains it exists
+        if current_user.id == participation.user_id || current_user.admin
+          if participation.destroy
+            render status: 200
+          else
+            render status: 500
+          end
+        else
+          render status: 403
+        end
+      else
+        render status: 404
+      end
+    else
+      render status: 401
+    end
+  end
 end
