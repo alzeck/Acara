b76c76c9d488dc96074aa305036240e13fb091f9
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-06-26 01:23:18 +0200

    added chat views

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -1,2 +1,20 @@
 module ChatsHelper
+  def receiver(chat)
+    if chat.user1 == current_user
+      chat.user2
+    else
+      chat.user1
+    end
+  end
+
+  def getAlignment(user)
+    if user == current_user
+      "ml-auto"
+    else
+      "mr-auto"
+    end
+  end
+
+
+
 end
