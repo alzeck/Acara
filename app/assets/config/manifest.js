7adcd9d4fc6014239ea056cfadef4ba191473f74
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-06-26 13:34:04 +0200

    remove unused links

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link jquery/dist/jquery.js
-//= link popper.js/dist/umd/popper.js
-//= link bootstrap/dist/js/bootstrap.js
 //= link_directory ../js .js
