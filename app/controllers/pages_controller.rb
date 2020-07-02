c107c348a3f5605f5159cbed089512f47de370c3
Matteo Di Stadio <distadio.1794111@studenti.uniroma1.it>
2020-07-02 19:26:14 +0200

    aggiunto un migliore filtro di ricerca eventi tramite data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 	#GET sulla root
 	def home
 		@hasGL = params.has_key?(:gl)
-		@events = helpers.pagesGeneral(nil, nil, helpers.getLocation, nil, 0)
+		@events = helpers.pagesGeneral(nil, nil, helpers.getLocation, nil, nil, 0)
 	end
 
 
@@ -11,23 +11,21 @@ class PagesController < ApplicationController
 	#Restituisce un json {"type": X, "content": Y}
 	#Con X "users", "events", "tags"
 	#Con Y rispettivamente la lista di utenti, eventi e un json { "tags": lista_tags, "events": lista_events_tags }
-	#TODO pass cords, start and end as @vars 
-	#TODO use w=cords, es=start and ee=end as params 
-	#TODO update search function to use an interval instead of a day 
 	def search
 		@hasGL = params.has_key?(:gl)
 
 		if !params.has_key?(:q) || params[:q].nil?
-			@risposta = { type: "events", content: helpers.pagesGeneral(nil, nil, helpers.getLocation, nil, 0) }
+			@risposta = { type: "events", content: helpers.pagesGeneral(nil, nil, helpers.getLocation, nil, nil, 0) }
 
 		else
 			ricerca = params[:q].downcase.strip
 			qualiFiltri = 0
 	
-			if params.has_key?(:dove)
-				dove = params[:dove]
+			if params.has_key?(:w)
+				dove = params[:w]
 	
 				if dove.match?(/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/)
+					@w = dove
 					dove = dove.split(",")
 					dove[0] = dove[0].to_d
 					dove[1] = dove[1].to_d
@@ -39,15 +37,20 @@ class PagesController < ApplicationController
 				dove = helpers.getLocation
 			end
 	
-			if params.has_key?(:quando)
+			if params.has_key?(:es) && params.has_key?(:ee)
 				begin
-					quando = params[:quando].to_datetime
+					@es = params[:es]
+					@ee = params[:ee]
+					inizio = params[:es].to_datetime
+					fine = params[:ee].to_datetime
 					qualiFiltri += 2
 				rescue ArgumentError
-					quando = nil
+					inizio = nil
+					fine = nil
 				end			
 			else
-				quando = nil
+				inizio = nil
+				fine = nil
 			end
 			
 			#cerco utenti
@@ -56,11 +59,11 @@ class PagesController < ApplicationController
 	
 			#cerco tag (e corrispettivi eventi)
 			elsif ricerca[0] == "#"
-				@risposta = { type: "tags", content: helpers.searchTags(ricerca, dove, quando, qualiFiltri) }
+				@risposta = { type: "tags", content: helpers.searchTags(ricerca, dove, inizio, fine, qualiFiltri) }
 	
 			#cerco eventi
 			else
-				@risposta = { type: "events", content: helpers.pagesGeneral(ricerca, nil, dove, quando, qualiFiltri) }
+				@risposta = { type: "events", content: helpers.pagesGeneral(ricerca, nil, dove, inizio, fine, qualiFiltri) }
 			end
 		end
 	end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -81,7 +81,7 @@ module PagesHelper
 
 
     #funzione per smistare ordinatamente gli eventi in base agli input forniti
-    def pagesGeneral(titolo, tag, dove, quando, qualiFiltri)
+    def pagesGeneral(titolo, tag, dove, inizio, fine, qualiFiltri)
         inZona_attivi_verificati_following = []
 		inZona_attivi_verificati_nonFollowing = []
 		inZona_attivi_nonVerificati_following = []
@@ -108,7 +108,8 @@ module PagesHelper
 			elemLoc[1] = elemLoc[1].to_d
 
 			pagesZona(
-				quando,
+                inizio,
+                fine,
 				dove,
 				elemLoc,
 				User.find(elem.user_id),
@@ -194,20 +195,20 @@ module PagesHelper
 
 
     #funzione che smista l'evento in base alla posizione passata
-    def pagesZona(date, loc, elemLoc, creator, event, i_a_v_f, i_a_v_nf, i_a_nv_f, i_a_nv_nf, i_i_v_f, i_i_v_nf, i_i_nv_f, i_i_nv_nf, n_a_v_f, n_a_v_nf, n_a_nv_f, n_a_nv_nf, n_i_v_f, n_i_v_nf, n_i_nv_f, n_i_nv_nf)
+    def pagesZona(inizio, fine, loc, elemLoc, creator, event, i_a_v_f, i_a_v_nf, i_a_nv_f, i_a_nv_nf, i_i_v_f, i_i_v_nf, i_i_nv_f, i_i_nv_nf, n_a_v_f, n_a_v_nf, n_a_nv_f, n_a_nv_nf, n_i_v_f, n_i_v_nf, n_i_nv_f, n_i_nv_nf)
         #usato come metro di paragone una distanza pari a circa il raggio di Roma (Italia) [circa 40km]
         if distance(elemLoc, loc) <= 20000
-            if date.nil?
+            if inizio.nil? || fine.nil?
                 pagesActive(creator, event, i_a_v_f, i_a_v_nf, i_a_nv_f, i_a_nv_nf, i_i_v_f, i_i_v_nf, i_i_nv_f, i_i_nv_nf)
             else
-                pagesDate(date, creator, event, i_a_v_f, i_a_v_nf, i_a_nv_f, i_a_nv_nf, i_i_v_f, i_i_v_nf, i_i_nv_f, i_i_nv_nf)
+                pagesDate(inizio, fine, creator, event, i_a_v_f, i_a_v_nf, i_a_nv_f, i_a_nv_nf, i_i_v_f, i_i_v_nf, i_i_nv_f, i_i_nv_nf)
             end
 
         else
-            if date.nil?
+            if inizio.nil? || fine.nil?
                 pagesActive(creator, event, n_a_v_f, n_a_v_nf, n_a_nv_f, n_a_nv_nf, n_i_v_f, n_i_v_nf, n_i_nv_f, n_i_nv_nf)
             else
-                pagesDate(date, creator, event, n_a_v_f, n_a_v_nf, n_a_nv_f, n_a_nv_nf, n_i_v_f, n_i_v_nf, n_i_nv_f, n_i_nv_nf)
+                pagesDate(inizio, fine, creator, event, n_a_v_f, n_a_v_nf, n_a_nv_f, n_a_nv_nf, n_i_v_f, n_i_v_nf, n_i_nv_f, n_i_nv_nf)
             end
         end
     end
@@ -224,8 +225,8 @@ module PagesHelper
 
 
     #funzione che smista l'evento in base alla sua data di inizio rispetto a quella cercata
-    def pagesDate(date, creator, event, a_v_f, a_v_nf, a_nv_f, a_nv_nf, i_v_f, i_v_nf, i_nv_f, i_nv_nf)
-        if event.start <= date + 1 && event.start >= date - 1
+    def pagesDate(inizio, fine, creator, event, a_v_f, a_v_nf, a_nv_f, a_nv_nf, i_v_f, i_v_nf, i_nv_f, i_nv_nf)
+        if event.start <= fine + 1 && event.end >= inizio
             pagesVerification(creator, event, a_v_f, a_v_nf, a_nv_f, a_nv_nf)
         else
             pagesVerification(creator, event, i_v_f, i_v_nf, i_nv_f, i_nv_nf)
@@ -289,9 +290,9 @@ module PagesHelper
 
 
     #funzione per la ricerca di tag (ed eventi correlati)
-    def searchTags(ricerca, dove, quando, qualiFiltri)
+    def searchTags(ricerca, dove, inizio, fine, qualiFiltri)
         tags = Tag.where("name ~* ?", ricerca[1, ricerca.length-1])
-        events = pagesGeneral(nil, ricerca, dove, quando, qualiFiltri)
+        events = pagesGeneral(nil, ricerca, dove, inizio, fine, qualiFiltri)
         return { tags: tags, events: events }
     end
 
