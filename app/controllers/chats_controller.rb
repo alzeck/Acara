f7be1c431431d30a40e99e3a22ca82b49c248948
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-07-08 21:44:10 +0200

    fix indentation

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -17,7 +17,7 @@ class ChatsController < ApplicationController
         @chat = Chat.find(params[:id])
         @message = Message.new chat: @chat
         if @chat.user1_id == current_user.id || @chat.user2_id == current_user.id
-          @chat.updateMessagesRead(current_user) # Update all the received messages as read 
+          @chat.updateMessagesRead(current_user) # Update all the received messages as read
           @messages = Message.where(chat_id: params[:id]).sort_by(&:created_at)
         else
           render_403
@@ -37,28 +37,28 @@ class ChatsController < ApplicationController
       # TODO check if this control is needed elsewhere
       if par.present?
         # verify if the needed param has been given ( avoid to)
-        user = par.to_i 
+        user = par.to_i
         primo_id = user < current_user.id ? user : current_user.id
         secondo_id = user > current_user.id ? user : current_user.id
 
         # Check if the chat already exists ( case user tries to access from profile )
         @chat = Chat.where(user1_id: primo_id, user2_id: secondo_id)[0]
         if @chat.present?
-        # if the chat exists redirect to
-        redirect_to chat_path(@chat)
-      else
-        # else try to create it
-        @chat = Chat.new(user1_id: primo_id, user2_id: secondo_id)
-        @chat.save!
-        if @chat.valid?
-          if @chat.save
-            redirect_to chat_path(@chat)
-          else
-            render_500
-          end
+          # if the chat exists redirect to
+          redirect_to chat_path(@chat)
         else
-          render_400
-        end
+          # else try to create it
+          @chat = Chat.new(user1_id: primo_id, user2_id: secondo_id)
+          @chat.save!
+          if @chat.valid?
+            if @chat.save
+              redirect_to chat_path(@chat)
+            else
+              render_500
+            end
+          else
+            render_400
+          end
         end
       else
         render_404
